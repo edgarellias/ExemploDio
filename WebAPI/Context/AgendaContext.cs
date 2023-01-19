50cabd1b7248f7dc8093cd23fe8ef85f87cf308a
Edgar Elias <edgar_jk3@hotmail.com>
2023-01-18 22:47:06 -0300

    Iniciando config entity framework

---
+++ WebAPI/Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
+
+namespace WebAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,11 @@
+using WebAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
